c6340bca5b59b3e16a7723607389e8f23199c842
Greg Oliver <divingclimber@gmail.com>
2017-12-07 11:14:40 -0800

    install.sh fix

---
+++ scripts/azureProviderAndCreds.tf
@@ -0,0 +1,17 @@
+#
+#
+# Provider and credential snippet to add to configurations
+# Assumes that there's a terraform.tfvars file with the var values
+#
+#
+variable subscription_id {}
+variable tenant_id {}
+variable client_id {}
+variable client_secret {}
+
+provider "azurerm" {
+    subscription_id = "${var.subscription_id}"
+    tenant_id = "${var.tenant_id}"
+    client_id = "${var.client_id}"
+    client_secret = "${var.client_secret}"
+}
